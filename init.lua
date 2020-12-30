7b78801eff32af2f1fb5a871ad762431969c8478
Leandro Facchinetti <git@leafac.com>
2020-12-30 08:55:30 +0000

    .

--- init.lua
+++ init.lua
@@ -98,10 +98,6 @@ do
         local window = hs.window.focusedWindow()
         window:moveToScreen(window:screen():next())
     end)
-    hs.hotkey.bind(configuration.modifiers, "R", function()
-        local screen = hs.screen.find("LG HDR 4K")
-        screen:rotate(screen:rotate() ~= 0 and 0 or 90)
-    end)
 end
 ::endWindowManagement::
 
